23992991e1714c9a775e55200c7cdd03ef856337
Leandro Ostera <leandro@ostera.io>
2020-10-02 08:49:01 +0200

    toolchains: sketch support for release archives

--- src/toolchains/archive.rs
+++ src/toolchains/archive.rs
@@ -6,10 +6,24 @@ use std::io::{Read, Write};
 use std::path::PathBuf;
 use std::process::Command;
 
+#[derive(Debug, Clone)]
+pub enum ArchiveKind {
+    Source,
+    Release,
+}
+
+impl Default for ArchiveKind {
+    fn default() -> Self {
+        ArchiveKind::Source
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct Archive {
+    kind: ArchiveKind,
     url: String,
     sha1: String,
+    name: String,
     prefix: String,
 }
 
@@ -22,7 +36,52 @@ impl Archive {
     }
 
     pub fn url(&self) -> String {
-        self.url.clone()
+        match self.kind {
+            ArchiveKind::Source => self.url.clone(),
+            ArchiveKind::Release => {
+                let parts: Vec<&str> = self.url.split("/tag/").collect();
+                let prefix = parts[0];
+                let tag = parts[1];
+                let suffix = match os_info::get().os_type() {
+                    os_info::Type::Macos => "x86_64-apple-darwin",
+                    os_info::Type::Alpine => "x86_64-linux-musl",
+                    os_info::Type::Arch
+                    | os_info::Type::CentOS
+                    | os_info::Type::Debian
+                    | os_info::Type::Fedora
+                    | os_info::Type::Linux
+                    | os_info::Type::Manjaro
+                    | os_info::Type::openSUSE
+                    | os_info::Type::OracleLinux
+                    | os_info::Type::Redhat
+                    | os_info::Type::RedHatEnterprise
+                    | os_info::Type::Solus
+                    | os_info::Type::SUSE
+                    | os_info::Type::Ubuntu => "x86_64-linux-gnu",
+                    os_info::Type::Windows => "x86_64-pc-windows",
+                    os_info::Type::Amazon
+                    | os_info::Type::Android
+                    | os_info::Type::Emscripten
+                    | os_info::Type::EndeavourOS
+                    | os_info::Type::Pop
+                    | os_info::Type::Redox
+                    | os_info::Type::Unknown
+                    | _ => "UNSUPPORTED_PLATFORM",
+                };
+                // NOTE: This constructs a URL like:
+                //  https://github.com/AbstractMachinesLab/caramel/releases/download/v0.0.4/caramel-v0.0.4-x86_64-linux-gnu.tar.gz
+                //  which means we support this sort of release tarball patterning.
+                // FIXME: just copy the actual pattern from the Rust toolchain
+                format!(
+                    "{}/download/{}/{}-{}-{}.tar.gz",
+                    prefix,
+                    tag.clone(),
+                    self.name.clone(),
+                    tag,
+                    suffix
+                )
+            }
+        }
     }
 
     pub fn sha1(&self) -> String {
@@ -31,6 +90,10 @@ impl Archive {
 
     pub fn prefix(&self) -> String {
         self.prefix.clone()
+    }
+
+    pub fn name(&self) -> String {
+        self.name.clone()
     }
 
     pub fn file_name(&self) -> String {
@@ -47,6 +110,35 @@ impl Archive {
 
     pub fn with_prefix(self, prefix: String) -> Archive {
         Archive { prefix, ..self }
+    }
+
+    pub fn with_name(self, name: String) -> Archive {
+        Archive { name, ..self }
+    }
+
+    pub fn as_source(self) -> Archive {
+        Archive {
+            kind: ArchiveKind::Source,
+            ..self
+        }
+    }
+
+    pub fn as_release(self) -> Archive {
+        Archive {
+            kind: ArchiveKind::Release,
+            ..self
+        }
+    }
+
+    pub fn validate(&self) {
+        match self.kind {
+            ArchiveKind::Release => {
+                if self.name.is_empty() {
+                    panic!("Release Archives MUST specify a name attribute!")
+                }
+            }
+            _ => (),
+        }
     }
 
     pub fn is_cached(&self, outdir: &PathBuf) -> Result<bool, anyhow::Error> {
@@ -85,7 +177,8 @@ impl Archive {
     pub fn download(&self, outdir: &PathBuf) -> Result<(), anyhow::Error> {
         info!(
             "Downloading toolchain from {:?} into {:?}",
-            &self.url, &outdir
+            self.url(),
+            &outdir
         );
 
         std::fs::create_dir_all(&outdir).context(format!(
@@ -94,7 +187,7 @@ impl Archive {
         ))?;
 
         let wget = Command::new("wget")
-            .args(&[self.url.clone()])
+            .args(&[self.url()])
             .args(&["-O", self.file_name().as_str()])
             .current_dir(&outdir)
             .output()
