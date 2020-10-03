f51ecfc5c707ebbfc9ea36a4a97b35148d1d7def
Leandro Ostera <leandro@ostera.io>
2020-10-03 21:37:30 +0200

    core: refactor labels to keep path separate from label name

--- src/label.rs
+++ src/label.rs
@@ -1,19 +1,16 @@
 use std::ops::Deref;
 use std::path::PathBuf;
 
+static WILDCARD: &str = "//...";
+
 #[derive(Debug, Clone, Hash, PartialEq)]
-pub struct Label {
-    name: String,
+pub enum Label {
+    Wildcard,
+    Relative { name: String },
+    Absolute { name: String, path: PathBuf },
 }
 
 impl Eq for Label {}
-
-impl Deref for Label {
-    type Target = String;
-    fn deref(&self) -> &String {
-        &self.name
-    }
-}
 
 impl From<&str> for Label {
     fn from(name: &str) -> Label {
@@ -29,13 +26,17 @@ impl From<String> for Label {
 
 impl ToString for Label {
     fn to_string(&self) -> String {
-        self.name.clone()
+        match self {
+            Label::Wildcard => WILDCARD.to_string(),
+            Label::Relative { name } => format!(":{}", name),
+            Label::Absolute { name, path } => format!("//{}:{}", path.to_str().unwrap(), name),
+        }
     }
 }
 
 impl Default for Label {
     fn default() -> Label {
-        "//...".into()
+        WILDCARD.into()
     }
 }
 
@@ -44,37 +45,76 @@ impl Label {
         let name = name.replace("\"", "");
 
         let is_wildcard = name.starts_with("//") && name.ends_with("...");
-        let is_relative = name.starts_with(':');
         let is_abs_name = name.starts_with("//") && name.contains(':');
 
-        let name = if is_relative || is_wildcard || is_abs_name {
-            name
-        } else {
-            format!(":{}", name)
+        if is_wildcard {
+            return Label::Wildcard;
+        }
+
+        if is_abs_name {
+            let parts: Vec<&str> = name.split(":").collect();
+            return Label::Absolute {
+                path: PathBuf::from(parts[0].strip_prefix("//").unwrap()),
+                name: parts[1].to_string(),
+            };
+        }
+
+        return Label::Relative {
+            name: if name.starts_with(":") {
+                name.strip_prefix(":").unwrap().to_string()
+            } else {
+                name
+            },
         };
-        Label { name }
+    }
+
+    pub fn name(&self) -> String {
+        match self {
+            Label::Wildcard => WILDCARD.to_string(),
+            Label::Relative { name } => name.clone(),
+            Label::Absolute { name, .. } => name.clone(),
+        }
+    }
+
+    pub fn path(&self) -> PathBuf {
+        match self {
+            Label::Wildcard => PathBuf::from("."),
+            Label::Relative { .. } => PathBuf::from("."),
+            Label::Absolute { path, .. } => path.to_path_buf(),
+        }
     }
 
     pub fn is_all(&self) -> bool {
-        self.name.starts_with("//...")
+        match self {
+            Label::Wildcard => true,
+            _ => false,
+        }
     }
 
     pub fn is_absolute(&self) -> bool {
-        self.name.starts_with("//")
+        match self {
+            Label::Absolute { .. } => true,
+            _ => false,
+        }
     }
 
     pub fn canonicalize(self, path: &PathBuf) -> Label {
-        if self.is_absolute() {
-            self
-        } else {
-            let path = path
-                .join(&self.name)
-                .to_str()
-                .unwrap()
-                .replace("./", "")
-                .replace("/:", ":");
-
-            format!("//{}", path).into()
+        match self {
+            Label::Relative { name } => Label::Absolute {
+                name,
+                path: if path.starts_with("./") {
+                    path.strip_prefix("./")
+                        .expect(&format!(
+                            "Could not remove relative prefix from relative path: {:?}",
+                            &path
+                        ))
+                        .clone()
+                        .to_path_buf()
+                } else {
+                    path.clone()
+                },
+            },
+            _ => self,
         }
     }
 }
