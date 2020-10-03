5d932a7f9075bd9b9f35334994266b612cc724d8
Leandro Ostera <leandro@ostera.io>
2020-10-03 21:44:37 +0200

    toolchain(caramel): simplified thanks to sandboxing

--- src/toolchains/caramel.rs
+++ src/toolchains/caramel.rs
@@ -19,6 +19,7 @@ impl Default for Toolchain {
     fn default() -> Toolchain {
         let archive = Archive::default()
             .as_release()
+            .with_name("caramel".to_string())
             .with_url(
                 "https://github.com/AbstractMachinesLab/caramel/releases/tag/v0.0.4".to_string(),
             )
@@ -68,57 +69,14 @@ impl Toolchain {
         "caramel".to_string()
     }
 
-    pub fn shell(self, code_paths: &[PathBuf]) -> Result<(), anyhow::Error> {
-        debug!("Starting shell with dependencies: {:?}", &code_paths);
-        let mut code_paths: Vec<String> = code_paths
-            .iter()
-            .map(|p| String::from(p.to_str().unwrap()))
-            .collect();
-        let mut args = vec![String::from("-pa")];
-        args.append(&mut code_paths);
-
-        let mut cmd = Command::new("erl");
-        let stdout = cmd
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .args(args.as_slice())
-            .spawn()
-            .context("Could not spawn erl")?
-            .stdout
-            .context("Could not capture standard output")?;
-        debug!("Calling {:?}", &cmd);
-
-        let reader = BufReader::new(stdout);
-
-        reader
-            .lines()
-            .filter_map(Result::ok)
-            .for_each(|line| println!("{}", line));
-
-        Ok(())
-    }
-
-    pub fn compile(
-        self,
-        caramel_srcs: &[PathBuf],
-        erl_srcs: &[PathBuf],
-        includes: &[PathBuf],
-        extra_libs: &[PathBuf],
-        dst: &PathBuf,
-    ) -> Result<(), anyhow::Error> {
-        /*
-         * Compile OCaml to Erlang
-         */
+    pub fn compile(self, caramel_srcs: &[PathBuf]) -> Result<(), anyhow::Error> {
         let caramel_srcs: Vec<&str> = caramel_srcs
             .iter()
             .map(|src| src.to_str().unwrap())
             .collect();
 
         let mut cmd = Command::new(self.caramelc);
-        let cmd = cmd
-            .args(&["compile"])
-            .args(&["--output-dir", dst.parent().unwrap().to_str().unwrap()])
-            .args(caramel_srcs.as_slice());
+        let cmd = cmd.args(&["compile"]).args(caramel_srcs.as_slice());
 
         debug!("Calling {:#?}", &cmd);
         let output = cmd.output()?;
@@ -127,53 +85,7 @@ impl Toolchain {
         } else {
             std::io::stdout().write_all(&output.stdout).unwrap();
             std::io::stderr().write_all(&output.stderr).unwrap();
-            Err(anyhow!("Error running erlc"))
-        }?;
-
-        /*
-         * Compile Erlang to BEAM now
-         */
-
-        let erl_paths: Vec<&str> = erl_srcs.iter().map(|src| src.to_str().unwrap()).collect();
-
-        let include_paths: HashSet<&str> = includes
-            .iter()
-            .map(|hrl| hrl.parent().unwrap().to_str().unwrap())
-            .collect();
-        let includes: Vec<&str> = include_paths
-            .iter()
-            .cloned()
-            .flat_map(|dir| vec!["-I", dir])
-            .collect();
-
-        let extra_lib_paths: HashSet<&str> = extra_libs
-            .iter()
-            .map(|beam| beam.parent().unwrap().to_str().unwrap())
-            .collect();
-        let extra_libs: Vec<&str> = extra_lib_paths
-            .iter()
-            .cloned()
-            .flat_map(|dir| vec!["-pa", dir])
-            .collect();
-
-        let mut cmd = Command::new("erlc");
-        let cmd = cmd
-            .args(&["-b", "beam", "-Wall", "-Werror"])
-            .args(includes.as_slice())
-            .args(extra_libs.as_slice())
-            .args(&["-o", dst.parent().unwrap().to_str().unwrap()])
-            .args(&["--"])
-            .args(erl_paths.as_slice());
-
-        debug!("Calling {:#?}", &cmd);
-        let output = cmd.output()?;
-
-        if output.status.success() {
-            Ok(())
-        } else {
-            std::io::stdout().write_all(&output.stdout).unwrap();
-            std::io::stderr().write_all(&output.stderr).unwrap();
-            Err(anyhow!("Error running erlc"))
+            Err(anyhow!("Error running caramelc"))
         }
     }
 }
