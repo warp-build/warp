ed8ffa4539a7c34625bbcf29b4f3e7eb54bc26b0
Leandro Ostera <leandro@ostera.io>
2020-10-03 21:47:08 +0200

    examples: add caramel example

--- examples/tiny_lib/math/math.erl
+++ examples/tiny_lib/math/math.erl
@@ -5,5 +5,6 @@
 test() ->
   A = mult:mult(2, 3), % from an erlang lib
   B = 'div':add(A),    % from a clojerl lib
-  C = add:add(B, B),   % from a gleam lib
-  'Elixir.Math.Pow':pow(2, C). % from an elixir lib
+  % C = add:add(B, B),   % from a gleam lib
+  D = 'Elixir.Math.Pow':pow(2, B), % from an elixir lib
+  inc:inc(D). % from a caramel lib
