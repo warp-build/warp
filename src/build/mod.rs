7f82c6ed21a7c0ac74526f4d59ff61acaf5d90c9
Leandro Ostera <leandro@ostera.io>
2020-10-04 21:34:59 +0200

    core: export new build structs

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -1,9 +1,15 @@
 mod build_artifact;
-mod build_context;
+mod build_cache;
+mod build_node;
 mod build_plan;
 mod build_rule;
+mod build_runner;
+mod build_sandbox;
 
 pub use self::build_artifact::*;
-pub use self::build_context::*;
+pub use self::build_cache::*;
+pub use self::build_node::*;
 pub use self::build_plan::*;
 pub use self::build_rule::*;
+pub use self::build_runner::*;
+pub use self::build_sandbox::*;
