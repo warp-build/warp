3728ca067213a303db9164928482087f2cabe485
Leandro Ostera <leandro@ostera.io>
2021-01-06 16:06:31 +0100

    buildscript: introduce JS/Deno runtime for writing Rules/Toolchains

---
+++ lib/zap-buildscript/src/lib.rs
@@ -0,0 +1,30 @@
+use anyhow::*;
+pub use deno_core;
+use log::*;
+use std::path::PathBuf;
+use std::rc::Rc;
+
+pub struct BuildScript {
+    pub runtime: deno_core::JsRuntime,
+}
+
+impl BuildScript {
+    pub fn new() -> Result<BuildScript, anyhow::Error> {
+        let rt_options = deno_core::RuntimeOptions {
+            module_loader: Some(Rc::new(deno_core::FsModuleLoader)),
+            ..Default::default()
+        };
+
+        let runtime = deno_core::JsRuntime::new(rt_options);
+
+        Ok(BuildScript { runtime })
+    }
+
+    pub async fn load(&mut self, module: PathBuf) -> Result<deno_core::ModuleId, anyhow::Error> {
+        trace!("Loading module at {:?}", &module);
+        let mod_specifier = deno_core::ModuleSpecifier::resolve_path(module.to_str().unwrap())?;
+        let mod_id = self.runtime.load_module(&mod_specifier, None).await?;
+        self.runtime.mod_evaluate(mod_id).await?;
+        Ok(mod_id)
+    }
+}
