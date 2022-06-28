822b383ea1fb2077f6ab75d4bb797ff8c0941b39
Leandro Ostera <leandro@ostera.io>
2022-06-28 22:09:35 +0200

    fix(rules): use deterministic prefixing for erlang toolchain

--- .zap/toolchains/erlang.js
+++ .zap/toolchains/erlang.js
@@ -7,7 +7,9 @@ const impl = ctx => {
 
   const output = "erlang.tar.gz"
 
-  const url = `https://github.com/erlang/otp/releases/download/OTP-${version}/otp_src_${version}.tar.gz`
+  const prefix = `otp_src_${version}`
+
+  const url = `https://github.com/erlang/otp/releases/download/OTP-${version}/${prefix}.tar.gz`
 
   ctx.action().download({ url, sha1, output })
 
@@ -16,17 +18,14 @@ const impl = ctx => {
   ctx.action().runShell({
     script: `#!/bin/bash -xe
 
-cd otp_src_*
+cd ${prefix}
 ./otp_build all
 
 `});
 
-  const binRoot = "bin";
+  const binRoot = `${prefix}/bin`;
   const ERLC = File.join(binRoot, "erlc");
   const ERL = File.join(binRoot, "erl");
-
-  ctx.action().setPermissions({ file: ERLC, executable: true })
-  ctx.action().setPermissions({ file: ERL, executable: true })
 
   ctx.action().declareOutputs([]);
 
