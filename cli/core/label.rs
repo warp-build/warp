ed846bb89293a20e2a7346355d46374094973c77
Leandro Ostera <leandro@ostera.io>
2022-07-02 16:05:22 +0200

    feat(core): support relative labels in subtrees
    Relative labels in subtrees allow us to make a target depend on another
    target that is _contained_ within the same directory subtree.
    
    For example, if `//a:lib` depends on `//a/b:lib`, it can write it out
    as:
    
    ```toml
    [[rule]]
    name = "rule"
    deps = [ "./b:lib" ]
    ```
    
    Which helps make target `a` relocatable, to other parts of the
    repository without having to juggle around the subtarget paths too much.
    
    This, in conjunction with some form of "visibility rules", can help make
    a monorepo much easier to refactor/maintain over time by preventing
    targets from depending on deeply nested targets.

--- cli/core/label.rs
+++ cli/core/label.rs
@@ -1,5 +1,5 @@
 use serde::*;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 static COLON: char = ':';
 static DOT: &str = ".";
@@ -9,7 +9,7 @@ static WILDCARD: &str = "//...";
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Deserialize)]
 pub enum Label {
     Wildcard,
-    Relative { name: String },
+    Relative { name: String, path: PathBuf },
     Absolute { name: String, path: PathBuf },
 }
 
@@ -29,7 +29,11 @@ impl ToString for Label {
     fn to_string(&self) -> String {
         match self {
             Label::Wildcard => WILDCARD.to_string(),
-            Label::Relative { name } => format!(":{}", name),
+            Label::Relative { name, path } => {
+                let path = path.to_str().unwrap();
+                let path = if path == "." { "" } else { path };
+                format!("{}:{}", path, name)
+            }
             Label::Absolute { name, path } => format!("//{}:{}", path.to_str().unwrap(), name),
         }
     }
@@ -46,12 +50,12 @@ impl Label {
         Label::default()
     }
 
-    pub fn from_path_and_name(path: &PathBuf, name: &str) -> Label {
+    pub fn from_path_and_name(path: &Path, name: &str) -> Label {
         Label::new(&format!("//{}:{}", path.to_str().unwrap(), name))
     }
 
     pub fn new(name: &str) -> Label {
-        let name = name.replace("\"", "");
+        let name = name.replace('\"', "");
 
         let is_wildcard = name.starts_with("//") && name.ends_with("...");
         let is_abs_name = name.starts_with("//");
@@ -60,13 +64,13 @@ impl Label {
             return Label::Wildcard;
         }
 
+        let parts: Vec<&str> = name.split(COLON).collect();
         if is_abs_name {
-            let parts: Vec<&str> = name.split(COLON).collect();
             if parts.len() == 1 {
                 let path = PathBuf::from(name.strip_prefix("//").unwrap());
                 return Label::Absolute {
                     name: path.file_name().unwrap().to_str().unwrap().to_string(),
-                    path: path,
+                    path,
                 };
             }
             return Label::Absolute {
@@ -75,19 +79,30 @@ impl Label {
             };
         }
 
-        Label::Relative {
-            name: if name.starts_with(COLON) {
-                name.strip_prefix(COLON).unwrap().to_string()
-            } else {
-                name
-            },
+        // local target = relative to this build file
+        if name.starts_with(COLON) {
+            Label::Relative {
+                path: PathBuf::from("."),
+                name: name.strip_prefix(COLON).unwrap().to_string(),
+            }
+        } else if parts.len() == 1 {
+            let path = PathBuf::from(name);
+            Label::Relative {
+                name: path.file_name().unwrap().to_str().unwrap().to_string(),
+                path,
+            }
+        } else {
+            Label::Relative {
+                name: parts[1].to_string(),
+                path: PathBuf::from(parts[0]),
+            }
         }
     }
 
     pub fn name(&self) -> String {
         match self {
             Label::Wildcard => WILDCARD.to_string(),
-            Label::Relative { name } => name.clone(),
+            Label::Relative { name, .. } => name.clone(),
             Label::Absolute { name, .. } => name.clone(),
         }
     }
@@ -95,48 +110,28 @@ impl Label {
     pub fn path(&self) -> PathBuf {
         match self {
             Label::Wildcard => PathBuf::from(DOT),
-            Label::Relative { .. } => PathBuf::from(DOT),
+            Label::Relative { path, .. } => path.to_path_buf(),
             Label::Absolute { path, .. } => path.to_path_buf(),
         }
     }
 
     pub fn is_all(&self) -> bool {
-        match self {
-            Label::Wildcard => true,
-            _ => false,
-        }
+        matches!(self, Label::Wildcard)
     }
 
     pub fn is_relative(&self) -> bool {
-        match self {
-            Label::Relative { .. } => true,
-            _ => false,
-        }
+        matches!(self, Label::Relative { .. })
     }
 
     pub fn is_absolute(&self) -> bool {
-        match self {
-            Label::Absolute { .. } | Label::Wildcard => true,
-            _ => false,
-        }
+        matches!(self, Label::Absolute { .. } | Label::Wildcard)
     }
 
-    pub fn canonicalize(&self, path: &PathBuf) -> Label {
+    pub fn canonicalize(&self, root: &Path) -> Label {
         match self {
-            Label::Relative { name } => Label::Absolute {
+            Label::Relative { name, path } => Label::Absolute {
                 name: name.clone(),
-                path: if path.starts_with("./") {
-                    path.strip_prefix("./")
-                        .unwrap_or_else(|_| {
-                            panic!(
-                                "Could not remove relative prefix from relative path: {:?}",
-                                &path
-                            )
-                        })
-                        .to_path_buf()
-                } else {
-                    path.clone()
-                },
+                path: root.join(path.strip_prefix(".").unwrap()),
             },
             _ => self.clone(),
         }
@@ -148,53 +143,84 @@ mod tests {
     use super::*;
 
     #[test]
-    fn parses_relative_paths() {
+    fn parses_local_paths() {
         let path = ":hello";
-        let l1 = Label::new(&path);
+        let l1 = Label::new(path);
         let l2: Label = path.into();
         let l3: Label = path.to_string().into();
         assert_eq!(path, l1.to_string());
-        assert_eq!(false, l1.is_absolute());
-        assert_eq!(false, l1.is_all());
+        assert!(!l1.is_absolute());
+        assert!(!l1.is_all());
         assert_eq!(path, l2.to_string());
-        assert_eq!(false, l2.is_absolute());
-        assert_eq!(false, l2.is_all());
+        assert!(!l2.is_absolute());
+        assert!(!l2.is_all());
         assert_eq!(path, l3.to_string());
-        assert_eq!(false, l3.is_absolute());
-        assert_eq!(false, l3.is_all());
+        assert!(!l3.is_absolute());
+        assert!(!l3.is_all());
+    }
+
+    #[test]
+    fn parses_relative_paths_with_explicit_target() {
+        let path = "./hello:world";
+        let l1 = Label::new(path);
+        assert_eq!(path, l1.to_string());
+        assert_eq!("./hello", l1.path().to_str().unwrap());
+        assert_eq!("world", l1.name());
+    }
+
+    #[test]
+    fn parses_relative_paths_with_implicit_target() {
+        let path = "./hello";
+        let l1 = Label::new(path);
+        assert_eq!("./hello:hello", l1.to_string());
+        assert_eq!("./hello", l1.path().to_str().unwrap());
+        assert_eq!("hello", l1.name());
+    }
+
+    #[test]
+    fn canonicalizes_relative_path_from_a_root() {
+        let root = PathBuf::from("my/path");
+        let path = "./hello/relative:world";
+        let l1 = Label::new(path);
+        assert_eq!(path, l1.to_string());
+        assert!(l1.is_relative());
+        assert_eq!(
+            l1.canonicalize(&root).to_string(),
+            "//my/path/hello/relative:world"
+        );
     }
 
     #[test]
     fn parses_absolute_paths() {
         let path = "//my/path:hello";
-        let l1 = Label::new(&path);
+        let l1 = Label::new(path);
         let l2: Label = path.into();
         let l3: Label = path.to_string().into();
         assert_eq!(path, l1.to_string());
-        assert_eq!(true, l1.is_absolute());
-        assert_eq!(false, l1.is_all());
+        assert!(l1.is_absolute());
+        assert!(!l1.is_all());
         assert_eq!(path, l2.to_string());
-        assert_eq!(true, l2.is_absolute());
-        assert_eq!(false, l2.is_all());
+        assert!(l2.is_absolute());
+        assert!(!l2.is_all());
         assert_eq!(path, l3.to_string());
-        assert_eq!(true, l3.is_absolute());
-        assert_eq!(false, l3.is_all());
+        assert!(l3.is_absolute());
+        assert!(!l3.is_all());
     }
 
     #[test]
     fn parses_wildcard_path() {
         let path = "//...";
-        let l1 = Label::new(&path);
+        let l1 = Label::new(path);
         let l2: Label = path.into();
         let l3: Label = path.to_string().into();
         assert_eq!(path, l1.to_string());
-        assert_eq!(true, l1.is_absolute());
-        assert_eq!(true, l1.is_all());
+        assert!(l1.is_absolute());
+        assert!(l1.is_all());
         assert_eq!(path, l2.to_string());
-        assert_eq!(true, l2.is_absolute());
-        assert_eq!(true, l2.is_all());
+        assert!(l2.is_absolute());
+        assert!(l2.is_all());
         assert_eq!(path, l3.to_string());
-        assert_eq!(true, l3.is_absolute());
-        assert_eq!(true, l3.is_all());
+        assert!(l3.is_absolute());
+        assert!(l3.is_all());
     }
 }
