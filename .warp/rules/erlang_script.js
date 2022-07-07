0b0a65825aa79e431e482df142740b6d4999da79
Leandro Ostera <leandro@ostera.io>
2022-07-08 00:55:02 +0200

    feat(rules): add rule to create executable erlang scripts

--- .warp/rules/erlang_script.js
+++ .warp/rules/erlang_script.js
@@ -1,28 +1,120 @@
-import ErlangToolchain, { BEAM_EXT } from "../toolchains/erlang.js";
+import ErlangToolchain, { BEAM_EXT, HEADER_EXT } from "../toolchains/erlang.js";
 
 const impl = (ctx) => {
-  const { label, name, srcs, headers, deps, main } = ctx.cfg();
+  const { ERLC, ESCRIPT } = ErlangToolchain.provides();
+  const { label, name, main, apps } = ctx.cfg();
 
-  const { ESCRIPT } = ErlangToolchain.provides();
+  const cwd = Label.path(label);
 
-  const runtime_srcs = srcs
-    .concat([main])
-    .concat(headers);
+  const transitiveDeps = ctx.transitiveDeps().flatMap(dep => dep.outs);
 
-  const run = `${Label.path(label)}/run_escript`;
+  const includePaths = transitiveDeps
+    .filter(path => path.endsWith(HEADER_EXT))
+    .unique();
 
-  ctx.action().declareOutputs([run, ...runtime_srcs]);
-  ctx.action().declareRunScript(run);
+  const extraLibPaths = transitiveDeps
+    .filter(path => path.endsWith(BEAM_EXT))
+    .unique();
+
+  const build = `${cwd}/${name}_escript_builder.erl`;
+  const run = `${main}script`;
+
+  /*
+   * NOTE(@ostera): to create the final escript we will use the `escript` module
+   * in Erlang itself. It's easier than figuring out the internal data format ourselves.
+   *
+   * So we will just template a tiny temporary Erlang module that will build
+   * this exact escript for us.
+   */
   ctx.action().writeFile({
-    dst: run,
-    data: `#!/bin/bash -e
+    dst: build,
+    data: `
+-module(${name}_escript_builder).
 
-${ESCRIPT} \
-  ${main} \
-  $(dirname "\${BASH_SOURCE[0]}")/${File.filename(main)} $*
+-export([main/1]).
 
-`,
+main(_argv) ->
+  % The raw list of files.
+  DepBeamFiles = [
+    ${
+      transitiveDeps
+      .map(dep => `"${dep}"`)
+      .join(",\n    ")
+    }
+  ],
+
+  AppNames = [
+    ${apps.map(dep => `<<"${dep}">>`).join(",\n    ")}
+  ],
+
+  Apps = lists:flatmap(fun (AppName) ->
+    AppFile = binary:bin_to_list(<<AppName/binary, ".app">>),
+    AppPath = case code:where_is_file(AppFile) of
+                non_existing -> erlang:throw(<<"Missing application: ", AppName/binary>>);
+                Path -> binary:list_to_bin(Path)
+              end,
+
+    AppRoot = filename:dirname(filename:dirname(AppPath)),
+    EbinPath = binary:bin_to_list(<<AppRoot/binary, "/ebin/*{app,beam}">>),
+    PrivPath = binary:bin_to_list(<<AppRoot/binary, "/priv/**/*">>),
+
+    Files = filelib:wildcard(EbinPath) ++ filelib:wildcard(PrivPath),
+
+    lists:map(fun (File) ->
+      {ok, Data} = file:read_file(File),
+      {filename:basename(File), Data}
+    end, Files)
+  end, AppNames),
+
+  % Read all the beam bytecode into {"hello.beam", <<bytecode>>} tuples.
+  DepBeams = lists:map(fun (BeamFile) ->
+    {ok, Data} = file:read_file(BeamFile),
+    {filename:basename(BeamFile), Data}
+  end, DepBeamFiles),
+
+  SrcFiles = [
+    ${
+      [main]
+      .map(dep => `"${dep}"`)
+      .join(",\n    ")
+    }
+  ],
+
+  % The file names with their contents compiled to bytecode.
+  SrcBeams = lists:map(fun (SrcFile) ->
+    {ok, _, BeamCode} = compile:file(SrcFile, [binary, debug_info]),
+    BeamFile = filename:join([filename:basename(SrcFile, ".erl"), ".beam"]),
+    {filename:basename(BeamFile), BeamCode}
+  end, SrcFiles),
+
+  Files = Apps ++ SrcBeams ++ DepBeams,
+
+  % The layout of our escript archive.
+  Cfg = [
+    shebang,
+    comment,
+    {archive, Files, []}
+  ],
+
+  {ok, Bin} = escript:create(binary, Cfg),
+  ok = file:write_file(<<"${File.filename(run)}">>, Bin).
+`
   });
+
+  /*
+   * NOTE(@ostera): to create the final escript archive, we actually have to
+   * go _into_ the folder.
+   */
+  ctx.action().runShell({
+    script: `#!/bin/bash -xe
+
+${ESCRIPT} ${build}
+mv ${File.filename(run)} ${run}
+
+` })
+
+  ctx.action().declareOutputs([run]);
+  ctx.action().declareRunScript(run);
   ctx.action().setPermissions({file: run, executable: true});
 };
 
@@ -34,14 +126,12 @@ export default Warp.Rule({
   cfg: {
     name: label(),
     main: file(),
-    srcs: [file()],
-    headers: [file()],
     deps: [label()],
+    apps: [string()],
   },
   defaults: {
-    srcs: ["*.erl"],
-    headers: ["*.hrl"],
-    deps: []
+    deps: [],
+    apps: ["kernel"],
   },
   toolchains: [ErlangToolchain],
 });
