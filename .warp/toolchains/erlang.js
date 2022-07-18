66f14a8e26ca9b04272c41f9725c90ee13a02809
Leandro Ostera <leandro@ostera.io>
2022-07-19 01:48:25 +0200

    feat(rules): make erlang toolchain use openssl toolchain

--- .warp/toolchains/erlang.js
+++ .warp/toolchains/erlang.js
@@ -1,9 +1,11 @@
+import OpenSSLToolchain from "./openssl.js";
+
 export const HEADER_EXT = ".hrl";
 export const BEAM_EXT = ".beam";
 export const ERL_EXT = ".erl";
 
 const impl = ctx => {
-  const { version, sha1, build_flags } = ctx.cfg();
+  const { version, sha1, configure_flags, make_flags, build_size } = ctx.cfg();
   const { host } = ctx.env();
 
   const output = "erlang.tar.gz"
@@ -20,32 +22,40 @@ const impl = ctx => {
     script: `#!/bin/bash -xe
 
 cd ${prefix}
-./otp_build all ${build_flags}
 
-CANONICAL_SYSTEM_NAME=$(sh ./erts/autoconf/config.guess)
+${
+  build_size === "tiny"
+  ? "OTP_TINY_BUILD=true"
+  : build_size === "small"
+  ? "OTP_SMALL_BUILD=true"
+  : ""
+}
 
-pushd bin;
-  ln -s $CANONICAL_SYSTEM_NAME ${host.triple}
-popd;
+export OTP_SMALL_BUILD OTP_TINY_BUILD
 
-pushd release;
-  ln -s $CANONICAL_SYSTEM_NAME ${host.triple}
-popd;
+mkdir dist
+
+./configure ${configure_flags.join(" ")} \
+  --with-ssl=${OpenSSLToolchain.provides().OPENSSL_HOME} \
+  --prefix=$(pwd)/dist || exit 1
+
+make all install ${make_flags.join(" ")} || exit 1
 
 `});
 
   ctx.action().declareOutputs([]);
 
-  const binRoot = `${prefix}/bin/${host.triple}`;
+  const root = `${prefix}/dist`;
+  const binRoot = `${prefix}/dist/bin`;
   ctx.provides({
     ERLC: File.join(binRoot, "erlc"),
     ERL: File.join(binRoot, "erl"),
     ESCRIPT: File.join(binRoot, "escript"),
     CT_RUN: File.join(binRoot, "ct_run"),
     DIALYZER: File.join(binRoot, "dialyzer"),
-    ERL_ROOT: `${prefix}/bin`,
-    INCLUDE_PATH: `${prefix}/release/${host.triple}/usr/include`,
-    LIB_PATH: `${prefix}/release/${host.triple}/lib`,
+    ERL_ROOT: binRoot,
+    INCLUDE_PATH: `${root}/lib/erlang/usr/include`,
+    LIB_PATH: `${root}/lib/erlang/usr/lib`,
   });
 };
 
@@ -56,9 +66,14 @@ export default Warp.Toolchain({
   cfg: {
     version: string(),
     sha1: string(),
-    build_flags: [string()],
+    build_size: string(),
+    configure_flags: [string()],
+    make_flags: [string()],
   },
   defaults: {
-    build_flags: ["-t"]
+    build_size: "tiny",
+    configure_flags: [],
+    make_flags: [],
   },
+  toolchains: [OpenSSLToolchain]
 });
