6c0c6a63191647bf8f321288cf70dc6cbca56965
Leandro Ostera <leandro@ostera.io>
2022-07-19 01:48:11 +0200

    feat(rules): add openssl toolchain

---
+++ .warp/toolchains/openssl.js
@@ -0,0 +1,44 @@
+const impl = ctx => {
+  const { version, sha1, build_flags } = ctx.cfg();
+  const { host } = ctx.env();
+
+  const output = "openssl.tar.gz"
+
+  const prefix = `openssl-${version}`
+
+  const url = `https://github.com/openssl/openssl/archive/${version}.tar.gz`
+
+  ctx.action().download({ url, sha1, output })
+
+  ctx.action().extract({ src: output, dst: "." })
+
+  ctx.action().runShell({
+    script: `#!/bin/bash -xe
+
+cd ${prefix}
+
+mkdir dist
+
+./config -no-tests --prefix=$(pwd)/dist || exit 1
+make -j || exit 1
+make install -j || exit 1
+
+`});
+
+  ctx.action().declareOutputs([]);
+
+  ctx.provides({
+    OPENSSL_HOME: `${prefix}/dist`
+  });
+};
+
+export default Warp.Toolchain({
+  name: "//warp.build/toolchains:openssl",
+  mnemonic: "OpenSSL",
+  impl,
+  cfg: {
+    version: string(),
+    sha1: string(),
+  },
+});
+
