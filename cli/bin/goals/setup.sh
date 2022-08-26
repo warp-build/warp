2440f17c99ec3cede3b642a1db45b3a2587ac689
Leandro Ostera <leandro@ostera.io>
2022-08-26 18:37:58 +0200

    fix: add logs throughout setup

--- cli/bin/goals/setup.sh
+++ cli/bin/goals/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xe
 
+
+readonly CURRENT_USER=$(whoami)
+
+echo "Beginning setup for ${CURRENT_USER}..."
+
 readonly APFS_UTIL=/System/Library/Filesystems/apfs.fs/Contents/Resources/apfs.util
 
 # 0. Config
@@ -11,6 +16,8 @@ readonly ROOT_DISK_IDENTIFIER=$(
   /usr/sbin/diskutil info -plist / \
     | xmllint --xpath "/plist/dict/key[text()='ParentWholeDisk']/following-sibling::string[1]/text()" -
 )
+echo "Found main disk at: ${ROOT_DISK_IDENTIFIER}"
+echo "Creating the Warp volume now..."
 
 # 2. Create a new volume
 readonly WARP_VOLUME_NAME=$(
@@ -22,8 +29,10 @@ readonly WARP_VOLUME_NAME=$(
 
 # 3. Get the new volume UUID
 readonly WARP_VOLUME_ID=$( ${APFS_UTIL} -k "${WARP_VOLUME_NAME}" )
+echo "Warp volume created with id: ${WARP_VOLUME_ID}"
 
 # 4. Create a Daemon that will mount the volume onto /warp
+echo "Setting up the Warp daemon..."
 readonly WARP_VOLUME_MOUNTD_DEST=/Library/LaunchDaemons/dev.abstractmachines.warp.store.plist
 sudo tee ${WARP_VOLUME_MOUNTD_DEST} >/dev/null <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -48,10 +57,11 @@ EOF
 
 sudo launchctl bootstrap system "$WARP_VOLUME_MOUNTD_DEST" || true
 sudo launchctl kickstart -k system/dev.abstractmachines.warp.store
+echo "Warp daemon set up!"
 
 # 5. Create the mount point on /warp  (fstab)
+echo "Mounting Warp volume onto ${WARP_MOUNT_POINT}..."
 echo -e "UUID=${WARP_VOLUME_ID} ${WARP_MOUNT_POINT} apfs rw,noauto,nobrowse,suid,owners" | sudo tee -a /etc/fstab >/dev/null
-
 
 # 6. Create the synthetic root level /warp pointer
 echo -e "${WARP_MOUNT_POINT}" | sed 's,/,,g' | sudo tee -a /etc/synthetic.conf >/dev/null
@@ -59,6 +69,11 @@ echo -e "${WARP_MOUNT_POINT}" | sed 's,/,,g' | sudo tee -a /etc/synthetic.conf >
 
 # 7. Mount the disk
 diskutil mount ${WARP_VOLUME_ID}
+echo "Warp volume mounted!"
 
 # 8. Give write permissions to /warp folder
-sudo chown -R $(whoami): /warp
+echo "Adjusting permissions..."
+sudo chown -R ${CURRENT_USER}: ${WARP_MOUNT_POINT}
+echo "Permissions set!"
+echo ""
+echo "This machine is Warp-capable now."
