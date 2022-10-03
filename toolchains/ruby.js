392579766dcf9ba7be20ba4a55fbaf8cae7d4248
Robin Moe <Robin@tempura.se>
2022-10-03 14:43:20 +0200

    feat: add ruby toolchain

---
+++ toolchains/ruby.js
@@ -0,0 +1,52 @@
+import OpenSSLToolchain from "https://pkgs.warp.build/toolchains/openssl.js";
+
+const impl = (ctx) => {
+  const { version, sha1 } = ctx.cfg();
+
+  const output = "ruby.tar.gz";
+  const prefix = `ruby-${version}`;
+
+  const url = `https://cache.ruby-lang.org/pub/ruby/${version.slice(0,3)}/ruby-${version}.tar.gz`;
+
+  ctx.action().download({ url, sha1, output });
+
+  ctx.action().extract({ src: output, dst: "." });
+
+  ctx.action().runShell({
+    script: `
+
+    cd ${prefix}
+
+    ./configure --prefix=$(pwd)/dist --with-openssl-dir=$OpenSSL_HOME
+
+    make -j
+
+    make install -j
+
+    `,
+  });
+
+  ctx
+    .action()
+    .declareOutputs([
+      `${prefix}/dist/bin`,
+      `${prefix}/dist/lib`,
+      `${prefix}/dist/include`,
+    ]);
+
+  ctx.provides({
+    ruby: `${prefix}/dist/bin/ruby`,
+    irb: `${prefix}/dist/bin/irb`,
+  });
+};
+
+export default Warp.Toolchain({
+  name: "https://pkgs.warp.build/toolchains/ruby",
+  mnemonic: "Ruby",
+  impl,
+  cfg: {
+    version: string(),
+    sha1: string(),
+  },
+  toolchains: [OpenSSLToolchain]
+});
