5269502d977ab4a0eb14c31bda75d6a7c7797302
Leandro Ostera <leandro@abstractmachines.dev>
2022-10-19 17:55:58 +0200

    fix(erlang): add spec for ends_with function

--- erlang/base/str.erl
+++ erlang/base/str.erl
@@ -2,10 +2,10 @@
 
 -export([ends_with/2]).
 
+-spec ends_with(binary(), binary()) -> bool().
 ends_with(Str, Suffix) ->
   case string:split(Str, Suffix) of
     [_Prefix, ""] -> true;
     [<<_Prefix/binary>>, <<>>] -> true;
     _ -> false
   end.
-
