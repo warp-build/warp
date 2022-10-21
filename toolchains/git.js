1c70369a9d6f486b22c4640e917c6fd23ab77388
Diogo Mafra <diogo@mafra.sh>
2022-10-21 16:48:48 -0300

    feat(toolchains): adding git toolchain

---
+++ toolchains/git.js
@@ -0,0 +1,49 @@
+const impl = (ctx) => {
+  const { host } = ctx.env();
+  const { version, sha1 } = ctx.cfg();
+
+  const url = `https://mirrors.edge.kernel.org/pub/software/scm/git/git-${version}.tar.gz`;
+
+  const prefix = `git-${version}`;
+  const output = "git.tar.gz";
+
+  ctx.action().download({ url, sha1, output });
+
+  ctx.action().extract({ src: output, dst: "." });
+
+  ctx.action().runShell({
+    script: `
+
+    cd ${prefix}
+
+    make configure
+
+    ./configure --prefix=$(pwd)/dist
+
+    make -j
+
+    make install -j
+
+    `,
+  });
+
+  ctx
+    .action()
+    .declareOutputs([
+      `${prefix}/dist/bin`,
+      `${prefix}/dist/libexec`,
+      `${prefix}/dist/share`,
+    ]);
+
+  ctx.provides({ git: `${prefix}/dist/bin/git` });
+};
+
+export default Warp.Toolchain({
+  name: "https://pkgs.warp.build/toolchains/git",
+  mnemonic: "Git",
+  impl,
+  cfg: {
+    sha1: string(),
+    version: string(),
+  },
+});
