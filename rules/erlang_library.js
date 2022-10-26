ebbaf8f5cf7f3a50ca96125a0d288c15e0e821ac
Leandro Ostera <leandro@abstractmachines.dev>
2022-10-26 16:18:37 +0200

    feat(erlang_library): be more lenient with src inputs

--- rules/erlang_library.js
+++ rules/erlang_library.js
@@ -3,11 +3,14 @@ import ErlangToolchain, {HEADER_EXT, BEAM_EXT, ERL_EXT} from "https://pkgs.warp.
 const impl = ctx => {
   const { name, deps, srcs, headers, behaviors } = ctx.cfg();
 
+  const srcsErl = srcs.filter(p => p.endsWith(ERL_EXT));
+  const srcsHrl = headers.concat(srcs).filter(p => p.endsWith(HEADER_EXT));
+
   const transitiveDeps = ctx.transitiveDeps().flatMap(dep => dep.outs);
 
   const includePaths = transitiveDeps
     .filter(path => path.endsWith(HEADER_EXT))
-    .concat(headers)
+    .concat(srcsHrl)
     .flatMap(path => ["-I", File.parent(path)])
     .unique();
 
@@ -20,17 +23,17 @@ const impl = ctx => {
   const finalSrcs = transitiveDeps
     .filter(path => path.endsWith(ERL_EXT))
     .concat(behaviors)
-    .concat(srcs);
+    .concat(srcsErl);
 
   const outputs = behaviors
-    .concat(srcs)
+    .concat(srcsErl)
     .map(erl => File.withExtension(erl, BEAM_EXT))
-    .concat(headers);
+    .concat(srcsHrl);
 
   ctx.action().declareOutputs(outputs);
 
   const paths = {};
-  srcs.forEach( src => {
+  srcsErl.forEach( src => {
     const parent = File.parent(src);
     paths[parent] = paths[parent] || [];
     paths[parent].push(src);
