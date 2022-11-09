852d807aa9ed1d1e75954904ed42cffc011d314f
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-09 20:03:41 +0100

    fix: simplify init and leave lifting to lift command

--- cli/bin/commands/init.rs
+++ cli/bin/commands/init.rs
@@ -4,22 +4,13 @@ use std::collections::BTreeMap;
 use structopt::StructOpt;
 use warp_core::*;
 
-use crate::reporter::StatusReporter;
-
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(
     name = "init",
     setting = structopt::clap::AppSettings::ColoredHelp,
     about = "initializes a new workspace"
 )]
-pub struct InitCommand {
-    #[structopt(
-        help = r"The amount of workers to use to execute any necessary build tasks.",
-        short = "w",
-        long = "max-workers"
-    )]
-    max_workers: Option<usize>,
-}
+pub struct InitCommand {}
 
 impl InitCommand {
     pub async fn run(&self, warp: WarpEngine) -> Result<(), anyhow::Error> {
@@ -45,15 +36,6 @@ impl InitCommand {
             .default(current_dir)
             .show_default(true)
             .interact_text()?;
-
-        /*
-        let use_git_hooks: bool = Input::with_theme(&theme)
-            .with_prompt("Should we run Warp before every commit?")
-            .default("yes".to_string())
-            .show_default(true)
-            .interact()?
-            == "yes";
-        */
 
         let toolchains_registry =
             ToolchainsRegistry::fetch(&paths, warp.event_channel.clone()).await?;
@@ -86,14 +68,6 @@ impl InitCommand {
             .build()?;
 
         let workspace_file = WorkspaceFile::builder()
-            .aliases({
-                let mut map = BTreeMap::new();
-                map.insert(
-                    "erlang".to_string(),
-                    "https://rules.warp.build/toolchains/erlang".to_string(),
-                );
-                map
-            })
             .workspace(workspace_config)
             .toolchains({
                 let mut map = BTreeMap::new();
@@ -123,63 +97,10 @@ impl InitCommand {
 
         workspace_file.write(&warp.invocation_dir).await?;
 
-        let lifters: Vec<Label> = toolchains.iter().cloned().flat_map(|t| t.lifter).collect();
-        if !lifters.is_empty() {
-            let mut warp = warp.initialize().await?;
-            let status_reporter =
-                StatusReporter::new(warp.event_channel.clone(), false, Goal::Build);
-            let (lifters, ()) = futures::future::join(
-                warp.execute(
-                    &lifters,
-                    BuildOpts {
-                        concurrency_limit: self.max_workers.unwrap_or_else(num_cpus::get),
-                        ..BuildOpts::default()
-                    },
-                ),
-                status_reporter.run(&lifters),
-            )
-            .await;
-            lifters?;
-            for build_result in warp
-                .get_results()
-                .iter()
-                .filter(|br| br.executable_target.rule.kind.is_runnable())
-            {
-                let cmd_result = CommandRunner::builder()
-                    .cwd(warp.invocation_dir.to_path_buf())
-                    .manifest(build_result.target_manifest.clone())
-                    .target(build_result.executable_target.clone())
-                    .stream_outputs(true)
-                    .sandboxed(false)
-                    .args(vec![])
-                    .build()?
-                    .run()
-                    .await?;
-
-                let lifted_sigs: LiftedSignatures =
-                    serde_json::from_slice(cmd_result.stdout.as_bytes()).map_err(|err| {
-                        SignatureStoreError::ParseError {
-                            err,
-                            json: cmd_result.stdout.clone(),
-                        }
-                    })?;
-
-                for sig in lifted_sigs.signatures {
-                    let mut local_label: LocalLabel = sig.file.clone().into();
-                    local_label.set_workspace(&warp.invocation_dir);
-
-                    let source = sig.source.clone();
-
-                    warp.signature_store()
-                        .save(&local_label, &source, sig)
-                        .await?;
-                }
-            }
-        }
-
         println!(
             r#"We are ready, here's a few commands for you to try:
 
+  warp lift    scan and build a code database for your project
   warp build   build your workspace incrementally
   warp test    test your workspace incrementally
 
