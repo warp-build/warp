3e4ef2913662f5436bacfbaeb99996b91fc00f3f
Leandro Ostera <leandro@abstractmachines.dev>
2022-11-09 11:27:50 +0100

    fix: let build opts change goals and target filters

--- cli/core/build_opts.rs
+++ cli/core/build_opts.rs
@@ -18,10 +18,23 @@ impl TargetFilter {
 
 #[derive(Default, Debug, Copy, Clone)]
 pub struct BuildOpts {
-    pub goal: Goal,
-    pub target_filter: TargetFilter,
     pub concurrency_limit: usize,
     pub experimental_file_mode: bool,
     pub experimental_force_rebuild: bool,
+    pub experimental_runtime_input_detection: bool,
     pub experimental_stream_analyzer_outputs: bool,
+    pub goal: Goal,
+    pub target_filter: TargetFilter,
+}
+
+impl BuildOpts {
+    pub fn with_goal(mut self, goal: Goal) -> Self {
+        self.goal = goal;
+        self
+    }
+
+    pub fn with_target_filter(mut self, target_filter: TargetFilter) -> Self {
+        self.target_filter = target_filter;
+        self
+    }
 }
