932a9e27658e23021edc32129dafc104f3a06276
Leandro Ostera <leandro@abstractmachines.dev>
2022-12-01 12:18:07 +0100

    feat(beam): support multiple rebar.config files

--- beam/analyzer/lib/analyzer/get_dependencies.ex
+++ beam/analyzer/lib/analyzer/get_dependencies.ex
@@ -112,162 +112,173 @@ defmodule Analyzer.GetDependencies do
   end
 
   defp do_get_rebar_config_deps(req) do
-    rebar_config = Path.join(req.workspace_root, "rebar.config")
+    deps =
+      for rebar_config <- Path.wildcard("#{req.workspace_root}/**/rebar.config") do
+        with {:ok, config} <- :file.consult(rebar_config) do
+          Logger.debug("Reading rebar3 deps at #{rebar_config}")
+          deps = Keyword.get(config, :deps, [])
 
-    with {:ok, config} <- :file.consult(rebar_config) do
-      Logger.debug("Reading rebar3 deps at #{rebar_config}")
-      deps = Keyword.get(config, :deps, [])
-
-      test_deps =
-        if "test" in req.profiles do
-          config |> Keyword.get(:profiles, []) |> Keyword.get(:test, []) |> Keyword.get(:deps, [])
-        else
-          []
-        end
-
-      deps = deps ++ test_deps
-
-      hex_config =
-        :hex_core.default_config()
-        |> Map.merge(%{
-          http_user_agent_fragment: "(tools.warp.build/beam/analyzer)",
-          http_adapter: {Analyzer.HexHttp, %{}}
-        })
-
-      dependencies =
-        for dep <- deps do
-          dep =
-            case dep do
-              {name, version, {:pkg, pkg_name}} ->
-                Build.Warp.Dependency.new(
-                  url: "https://hex.pm/packages/#{pkg_name |> Atom.to_string()}",
-                  name: name |> Atom.to_string(),
-                  version: version |> :binary.list_to_bin(),
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_resolver: "https://tools.warp.build/hexpm/resolver"
-                )
-
-              {name, _version, {:git, repo, ref}} ->
-                ref =
-                  case ref do
-                    {:ref, ref} -> ref
-                    {:branch, branch} -> branch
-                    {:tag, tag} -> tag
-                    _ -> ref
-                  end
-
-                ref =
-                  cond do
-                    is_atom(ref) -> Atom.to_string(ref)
-                    true -> :binary.list_to_bin(ref)
-                  end
-
-                repo = :binary.list_to_bin(repo)
-
-                Build.Warp.Dependency.new(
-                  url: String.replace(repo, ".git", ""),
-                  name: name |> Atom.to_string(),
-                  version: ref,
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_resolver:
-                    if String.contains?(repo, "github") do
-                      "https://tools.warp.build/github/resolver"
-                    else
-                      "https://tools.warp.build/gitlab/resolver"
-                    end
-                )
-
-              {name, {:git, repo, ref}} ->
-                ref =
-                  case ref do
-                    {:ref, ref} -> ref
-                    {:branch, branch} -> branch
-                    {:tag, tag} -> tag
-                  end
-
-                ref =
-                  cond do
-                    is_atom(ref) -> Atom.to_string(ref)
-                    true -> :binary.list_to_bin(ref)
-                  end
-
-                repo = :binary.list_to_bin(repo)
-
-                Build.Warp.Dependency.new(
-                  url: String.replace(repo, ".git", ""),
-                  name: name |> Atom.to_string(),
-                  version: ref,
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_resolver:
-                    if String.contains?(repo, "github") do
-                      "https://tools.warp.build/github/resolver"
-                    else
-                      "https://tools.warp.build/gitlab/resolver"
-                    end
-                )
-
-              {name, {:git_subdir, repo, ref, subdir}} ->
-                ref =
-                  case ref do
-                    {:ref, ref} -> ref
-                    {:branch, branch} -> branch
-                    {:tag, tag} -> tag
-                  end
-
-                ref =
-                  cond do
-                    is_atom(ref) -> Atom.to_string(ref)
-                    true -> :binary.list_to_bin(ref)
-                  end
-
-                repo = :binary.list_to_bin(repo)
-
-                Build.Warp.Dependency.new(
-                  url: String.replace(repo, ".git", ""),
-                  name: name |> Atom.to_string(),
-                  version: ref,
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_subdir: subdir |> :binary.list_to_bin(),
-                  archive_resolver:
-                    if String.contains?(repo, "github") do
-                      "https://tools.warp.build/github/resolver"
-                    else
-                      "https://tools.warp.build/gitlab/resolver"
-                    end
-                )
-
-              {name, version} ->
-                Build.Warp.Dependency.new(
-                  url: "https://hex.pm/packages/#{name |> Atom.to_string()}",
-                  name: name |> Atom.to_string(),
-                  version: version |> :binary.list_to_bin(),
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_resolver: "https://tools.warp.build/hexpm/resolver"
-                )
-
-              name when is_atom(name) ->
-                {:ok, {_, _, resp}} = :hex_api_package.get(hex_config, Atom.to_string(name))
-
-                Build.Warp.Dependency.new(
-                  url: "https://hex.pm/packages/#{name |> Atom.to_string()}",
-                  name: name |> Atom.to_string(),
-                  version: resp["latest_stable_version"] || resp["latest_version"],
-                  signature_resolver: "https://tools.warp.build/hexpm/resolver",
-                  archive_resolver: "https://tools.warp.build/hexpm/resolver"
-                )
+          test_deps =
+            if "test" in req.profiles do
+              config
+              |> Keyword.get(:profiles, [])
+              |> Keyword.get(:test, [])
+              |> Keyword.get(:deps, [])
+            else
+              []
             end
 
-          dep
+          deps = deps ++ test_deps
+
+          hex_config =
+            :hex_core.default_config()
+            |> Map.merge(%{
+              http_user_agent_fragment: "(tools.warp.build/beam/analyzer)",
+              http_adapter: {Analyzer.HexHttp, %{}}
+            })
+
+          dependencies =
+            for dep <- deps do
+              dep =
+                case dep do
+                  {name, version, {:pkg, pkg_name}} ->
+                    Build.Warp.Dependency.new(
+                      url: "https://hex.pm/packages/#{pkg_name |> Atom.to_string()}",
+                      name: name |> Atom.to_string(),
+                      version: version |> :binary.list_to_bin(),
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_resolver: "https://tools.warp.build/hexpm/resolver"
+                    )
+
+                  {name, _version, {:git, repo, ref}} ->
+                    ref =
+                      case ref do
+                        {:ref, ref} -> ref
+                        {:branch, branch} -> branch
+                        {:tag, tag} -> tag
+                        _ -> ref
+                      end
+
+                    ref =
+                      cond do
+                        is_atom(ref) -> Atom.to_string(ref)
+                        true -> :binary.list_to_bin(ref)
+                      end
+
+                    repo = :binary.list_to_bin(repo)
+
+                    Build.Warp.Dependency.new(
+                      url: String.replace(repo, ".git", ""),
+                      name: name |> Atom.to_string(),
+                      version: ref,
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_resolver:
+                        if String.contains?(repo, "github") do
+                          "https://tools.warp.build/github/resolver"
+                        else
+                          "https://tools.warp.build/gitlab/resolver"
+                        end
+                    )
+
+                  {name, {:git, repo, ref}} ->
+                    ref =
+                      case ref do
+                        {:ref, ref} -> ref
+                        {:branch, branch} -> branch
+                        {:tag, tag} -> tag
+                      end
+
+                    ref =
+                      cond do
+                        is_atom(ref) -> Atom.to_string(ref)
+                        true -> :binary.list_to_bin(ref)
+                      end
+
+                    repo = :binary.list_to_bin(repo)
+
+                    Build.Warp.Dependency.new(
+                      url: String.replace(repo, ".git", ""),
+                      name: name |> Atom.to_string(),
+                      version: ref,
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_resolver:
+                        if String.contains?(repo, "github") do
+                          "https://tools.warp.build/github/resolver"
+                        else
+                          "https://tools.warp.build/gitlab/resolver"
+                        end
+                    )
+
+                  {name, {:git_subdir, repo, ref, subdir}} ->
+                    ref =
+                      case ref do
+                        {:ref, ref} -> ref
+                        {:branch, branch} -> branch
+                        {:tag, tag} -> tag
+                      end
+
+                    ref =
+                      cond do
+                        is_atom(ref) -> Atom.to_string(ref)
+                        true -> :binary.list_to_bin(ref)
+                      end
+
+                    repo = :binary.list_to_bin(repo)
+
+                    Build.Warp.Dependency.new(
+                      url: String.replace(repo, ".git", ""),
+                      name: name |> Atom.to_string(),
+                      version: ref,
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_subdir: subdir |> :binary.list_to_bin(),
+                      archive_resolver:
+                        if String.contains?(repo, "github") do
+                          "https://tools.warp.build/github/resolver"
+                        else
+                          "https://tools.warp.build/gitlab/resolver"
+                        end
+                    )
+
+                  {name, {:path, path}} ->
+                    nil
+
+                  {name, version} ->
+                    Build.Warp.Dependency.new(
+                      url: "https://hex.pm/packages/#{name |> Atom.to_string()}",
+                      name: name |> Atom.to_string(),
+                      version: version |> :binary.list_to_bin(),
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_resolver: "https://tools.warp.build/hexpm/resolver"
+                    )
+
+                  name when is_atom(name) ->
+                    {:ok, {_, _, resp}} = :hex_api_package.get(hex_config, Atom.to_string(name))
+
+                    Build.Warp.Dependency.new(
+                      url: "https://hex.pm/packages/#{name |> Atom.to_string()}",
+                      name: name |> Atom.to_string(),
+                      version: resp["latest_stable_version"] || resp["latest_version"],
+                      signature_resolver: "https://tools.warp.build/hexpm/resolver",
+                      archive_resolver: "https://tools.warp.build/hexpm/resolver"
+                    )
+                end
+
+              dep
+            end
+            |> Enum.filter(fn x -> not is_nil(x) end)
+
+          Logger.debug("rebar3 config had #{Enum.count(dependencies)} dependencies")
+
+          dependencies
+        else
+          e ->
+            Logger.error("Error reading rebar3 deps: #{inspect(e)}")
+            []
         end
+      end
+      |> List.flatten()
 
-      Logger.debug("rebar3 config had #{Enum.count(dependencies)} dependencies")
-
-      {:ok, dependencies}
-    else
-      e ->
-        Logger.error("Error reading rebar3 deps: #{inspect(e)}")
-        {:ok, []}
-    end
+    {:ok, deps}
   end
 
   defp do_get_rebar_lock_deps(req) do
