46460658e6af3d1a5b453bc2c109f144761c4076
Leandro Ostera <leandro@abstractmachines.dev>
2023-02-23 12:16:07 -0300

    test(MarkII): begin integration tests for the drive section

---
+++ cli/mark2/tests/warp_drive_tests.rs
@@ -0,0 +1,13 @@
+mod common;
+
+#[tokio::test]
+async fn current_workspace_is_loaded() {}
+
+#[tokio::test]
+async fn when_there_are_no_targets() {}
+
+#[tokio::test]
+async fn when_we_are_in_the_workspace_root() {}
+
+#[tokio::test]
+async fn when_we_need_to_move_to_the_workspace_root() {}
