141cabe2f3ad78b388ad09bf98e8f087aeabd18e
Leandro Ostera <leandro@abstractmachines.dev>
2023-02-23 12:14:36 -0300

    feat(MarkII): set default values on the config at build time

--- cli/mark2/config/mod.rs
+++ cli/mark2/config/mod.rs
@@ -17,31 +17,44 @@ pub const WARPFILE: &str = "Warpfile";
 #[builder(build_fn(error = "ConfigError"))]
 pub struct Config {
     /// Never access the network, but continue working if possible.
+    #[builder(default = "self.default_offline()")]
     offline: bool,
 
     /// The current working directory of warp
+    #[builder(default = "self.default_invocation_dir()?")]
     invocation_dir: PathBuf,
 
     /// The current user executing warp
+    #[builder(default = "self.default_current_user()")]
     current_user: String,
 
     /// The time at which this configuration was created. We will use this to compute the total
     /// build time.
+    #[builder(default = "self.default_created_at()")]
     created_at: Instant,
 
     /// The environment at the time this config was created. This is separated to assist with
     /// testing.
+    #[builder(default = "self.default_env()?")]
     env: HashMap<String, String>,
 
     /// The uppermost directory in the file system in which to search for config files.
-    #[builder(setter(into, strip_option))]
+    #[builder(setter(into, strip_option), default = "None")]
     search_stop_path: Option<PathBuf>,
 
     /// The maximum number of local workers to spawn in the local worker pool.
+    #[builder(default = "self.default_max_workers()")]
     max_local_workers: usize,
 
     /// The root of warp's operating directory. By default this is `/warp`
+    #[builder(default = "self.default_warp_root()")]
     warp_root: PathBuf,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self::builder().build().unwrap()
+    }
 }
 
 impl Config {
@@ -83,31 +96,31 @@ impl Config {
 }
 
 impl ConfigBuilder {
-    fn default_warp_root() -> PathBuf {
+    fn default_warp_root(&self) -> PathBuf {
         PathBuf::from("/warp")
     }
 
-    fn default_offline() -> bool {
+    fn default_offline(&self) -> bool {
         false
     }
 
-    fn default_max_workers() -> usize {
+    fn default_max_workers(&self) -> usize {
         num_cpus::get()
     }
 
-    fn default_current_user() -> String {
+    fn default_current_user(&self) -> String {
         whoami::username()
     }
 
-    fn default_invocation_dir() -> Result<PathBuf, ConfigError> {
+    fn default_invocation_dir(&self) -> Result<PathBuf, ConfigError> {
         std::env::current_dir().map_err(ConfigError::CouldNotGetCurrentDir)
     }
 
-    fn default_created_at() -> Instant {
+    fn default_created_at(&self) -> Instant {
         Instant::now()
     }
 
-    fn default_env() -> Result<HashMap<String, String>, ConfigError> {
+    fn default_env(&self) -> Result<HashMap<String, String>, ConfigError> {
         let env = std::env::vars_os()
             .filter_map(|(k, v)| match (k.into_string(), v.into_string()) {
                 (Ok(k), Ok(v)) => Some((k, v)),
