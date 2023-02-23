84edd92eba41e1a9dd29eeb35915afba45a783df
Leandro Ostera <leandro@abstractmachines.dev>
2023-02-23 12:06:46 -0300

    feat(MarkII): prepare for concurrent testing with shuttle

---
+++ cli/mark2/sync/mod.rs
@@ -0,0 +1,8 @@
+//! Wrapper over standard sync primitives to allow for concurrent testing.
+//!
+
+#[cfg(all(shuttle, test))]
+pub(crate) use shuttle::{sync::*, thread};
+
+#[cfg(not(all(shuttle, test)))]
+pub(crate) use std::{sync::*, thread};
