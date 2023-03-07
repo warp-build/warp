e846ae2c6025f76cd59833502d096328ed65b77e
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-07 11:29:30 -0300

    test(MarkII): planner will find deps

--- mark2/planner/default.rs
+++ mark2/planner/default.rs
@@ -119,7 +119,7 @@ mod tests {
     use crate::model::ConcreteTarget;
     use crate::resolver::TargetRegistry;
     use crate::rules::RuleExecutorError;
-    use crate::store::DefaultStore;
+    use crate::store::{ArtifactManifest, DefaultStore};
     use crate::sync::Arc;
     use crate::worker::TaskResults;
     use crate::{Config, Goal};
@@ -168,6 +168,50 @@ mod tests {
         let flow = p.plan(sig.clone(), env).await.unwrap();
 
         assert_matches!(flow, PlanningFlow::Planned { spec } if spec.target() == sig.target());
+    }
+
+    #[tokio::test]
+    async fn finds_built_dependencies() {
+        let config = Config::builder().build().unwrap();
+        let archive_manager = ArchiveManager::new(&config).into();
+        let store = DefaultStore::new(config, archive_manager).into();
+        let target_registry = Arc::new(TargetRegistry::new());
+        let task_results = TaskResults::new(target_registry.clone()).into();
+        let ctx = DefaultPlannerContext::new(store, target_registry, task_results);
+
+        let goal = Goal::Build;
+        let target = Arc::new("./my/file.ex".into());
+        let target_id = ctx.target_registry.register_target(&target);
+        let target = ConcreteTarget::new(goal, target_id, target, "".into());
+
+        let dep_target = Arc::new("./my/dep.ex".into());
+        let dep_target_id = ctx.target_registry.register_target(&dep_target);
+        let dep_target = ConcreteTarget::new(goal, dep_target_id, dep_target, "".into());
+
+        let dep_spec = ExecutableSpec::builder()
+            .target(dep_target.clone())
+            .build()
+            .unwrap();
+        let dep_manifest = ArtifactManifest::default();
+        ctx.task_results
+            .add_task_result(dep_target_id, dep_spec, dep_manifest);
+
+        let sig = Signature::builder()
+            .rule("test_rule".into())
+            .target(target.clone())
+            .deps(vec![(*dep_target.original_target()).clone()])
+            .build()
+            .unwrap();
+
+        let env = ExecutionEnvironment::default();
+
+        let mut p: DefaultPlanner<NoopRuleExecutor> = DefaultPlanner::new(ctx).unwrap();
+        let flow = p.plan(sig.clone(), env).await.unwrap();
+
+        assert_matches!(flow, PlanningFlow::Planned { spec } => {
+            assert_eq!(*spec.target(), target);
+            assert_eq!(*spec.deps().compile_deps().get(0).unwrap(), dep_target_id);
+        });
     }
 
     #[tokio::test]
