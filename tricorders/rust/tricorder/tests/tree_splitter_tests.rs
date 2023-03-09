15e71009795041c0f597088568f3f6fe2c7b9c39
Calin Capitanu <calincapitanu@gmail.com>
2023-03-09 23:07:59 +0100

    fix(rust): fix broken test

--- tricorders/rust/tricorder/tests/tree_splitter_tests.rs
+++ tricorders/rust/tricorder/tests/tree_splitter_tests.rs
@@ -4,8 +4,7 @@ use tricorder::TreeSplitter;
 fn t001_simple_fn_graph() {
     let src = include_str!("./sources/t001_simple_fn_graph.rs");
     let symbol = "main";
-    let ast = TreeSplitter::tree_split(symbol, src);
+    let (ast, filtered_source) = TreeSplitter::tree_split(symbol, src);
     insta::assert_debug_snapshot!(ast);
-    let formatted = prettyplease::unparse(&ast);
-    insta::assert_display_snapshot!(format!("{}", &formatted));
+    insta::assert_display_snapshot!(format!("{}", &filtered_source));
 }
