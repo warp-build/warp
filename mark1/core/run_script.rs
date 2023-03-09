2e69c1035d5965cbedc77632385ae7cebbdae797
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-08 23:49:00 -0300

    chore(MarkI): drop!

--- mark1/core/run_script.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct RunScript {
-    pub env: std::collections::HashMap<String, String>,
-    pub run_script: std::path::PathBuf,
-}
--- mark1/core/workspace_lifter.rs
+++
@@ -1,7 +0,0 @@
-pub struct WorkspaceLifter {}
-
-impl WorkspaceLifter {
-    pub fn new(workspace: &Workspace, toolchains_registry: Arc<ToolchainsRegistry>) -> Self {
-        Self {}
-    }
-}
