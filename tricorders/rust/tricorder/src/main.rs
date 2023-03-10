d3ece1df7652485fd8e875ca1b7329c6915f9d47
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-09 21:01:36 -0300

    fix(tricorders/rust): take server port as argument

--- tricorders/rust/tricorder/src/main.rs
+++ tricorders/rust/tricorder/src/main.rs
@@ -12,7 +12,9 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port = 50051;
+    // NOTE(@ostera): invokation is "tricorder start <port>"
+    let port: i32 = std::env::args().nth(2).unwrap().parse().unwrap();
+
     let addr = format!("0.0.0.0:{}", port).parse().unwrap();
     let server = AnalyzerServiceImpl::default();
 
