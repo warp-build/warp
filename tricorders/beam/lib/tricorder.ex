7a82c3a507d6cbcffefc84144fb452f9badf7013
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-11 15:49:18 -0300

    feat(cli): make room for the bootstrap command

---
+++ tricorders/beam/lib/tricorder.ex
@@ -0,0 +1,18 @@
+defmodule Tricorder do
+  @moduledoc """
+  Documentation for `Tricorder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tricorder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
