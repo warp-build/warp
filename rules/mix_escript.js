a79af382b5f519218f1f5932eeee793c3f968a53
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-12 14:40:50 -0300

    feat(mix_escript): create new escript rule

---
+++ rules/mix_escript.js
@@ -0,0 +1,48 @@
+import ElixirToolchain from "https://rules.warp.build/toolchains/elixir.js";
+import ErlangToolchain from "https://rules.warp.build/toolchains/erlang.js";
+
+const RULE_NAME = "https://rules.warp.build/rules/mix_escript";
+
+const impl = (ctx) => {
+  const { cwd, bin } = ctx.cfg();
+
+  const binPath = `${cwd()}/${bin}`;
+  const outputs = [ binPath ];
+
+  ctx.action().declareOutputs(outputs);
+  ctx.provides({ [bin]: binPath });
+
+  ctx.action().runShell({
+    env: { MIX_ENV: "default" },
+    script: `
+    cd ${cwd()}
+    mix deps.get --force
+    mix escript.build
+    `,
+  });
+};
+
+export default Warp.Rule({
+  name: RULE_NAME,
+  mnemonic: "MixScript",
+  impl,
+  cfg: {
+    name: target(),
+    bin: string(),
+    srcs: [file()],
+  },
+  defaults: {
+    srcs: [
+      "lib/**/*.eex",
+      "lib/**/*.ex",
+      "test/**/*.exs",
+      "mix.exs",
+      "src/**/*.app.src",
+      "src/**/*.erl",
+      "src/**/*.hrl",
+      "include/**/*.hrl",
+      "priv/**/*",
+    ],
+  },
+  toolchains: [ElixirToolchain, ErlangToolchain],
+});
