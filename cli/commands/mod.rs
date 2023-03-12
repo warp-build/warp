adb2d34774e4f399ec0a4a3ccb070b72ec853c83
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-12 18:42:25 -0300

    feat(cli): allow overrides on the public store metadata url

--- cli/commands/mod.rs
+++ cli/commands/mod.rs
@@ -11,9 +11,10 @@ pub use setup::*;
 pub use test::*;
 
 use structopt::StructOpt;
+use url::Url;
 use warp_core::Config;
 
-#[derive(Default, Debug, Clone, Copy, StructOpt)]
+#[derive(Default, Debug, Clone, StructOpt)]
 pub struct Flags {
     #[structopt(
         help = r"Never access the network, but continue working if possible.",
@@ -27,14 +28,23 @@ pub struct Flags {
         long = "max-localworkers"
     )]
     pub(crate) max_local_workers: Option<usize>,
+
+    #[structopt(long = "public-store-metadata-url")]
+    pub(crate) public_store_metadata_url: Option<Url>,
 }
 
 impl From<Flags> for Config {
     fn from(flags: Flags) -> Self {
-        Config::builder()
+        let mut config = Config::builder();
+
+        config
             .offline(flags.offline.unwrap_or(false))
-            .max_local_workers(flags.max_local_workers.unwrap_or_else(num_cpus::get))
-            .build()
-            .unwrap()
+            .max_local_workers(flags.max_local_workers.unwrap_or_else(num_cpus::get));
+
+        if let Some(url) = flags.public_store_metadata_url {
+            config.public_store_metadata_url(url);
+        }
+
+        config.build().unwrap()
     }
 }
