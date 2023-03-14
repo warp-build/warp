9067604f91445f3c534e59c909ea520185b35e7e
Calin Capitanu <calincapitanu@gmail.com>
2023-03-14 01:07:25 +0100

    feat(cli): print hashes on bootstrap

--- cli/commands/bootstrap.rs
+++ cli/commands/bootstrap.rs
@@ -15,14 +15,33 @@ pub struct BootstrapCommand {
 
 impl BootstrapCommand {
     pub async fn run(self) -> Result<(), anyhow::Error> {
-        let mut warp = WarpDriveMarkII::new(self.flags.into()).await?;
+        let mut warp = WarpDriveMarkII::new(self.flags.clone().into()).await?;
 
         let targets: Vec<Target> = vec![
             "./tricorders/beam/mix.exs".into(),
             "./tricorders/rust/Cargo.toml".into(),
         ];
 
-        let _results = warp.execute(Goal::Bootstrap, &targets).await?;
+        let results = warp.execute(Goal::Bootstrap, &targets).await?;
+
+        if self.flags.print_hashes {
+            println!();
+            println!("Targets:");
+            let mut results = results.get_results();
+            results.sort_by(|a, b| {
+                a.artifact_manifest
+                    .buildstamps()
+                    .build_started_at
+                    .cmp(&b.artifact_manifest.buildstamps().build_started_at)
+            });
+
+            for result in results {
+                let hash = result.artifact_manifest.hash();
+                let target = result.artifact_manifest.target();
+                println!("  {hash} => {target}");
+            }
+            println!();
+        }
 
         Ok(())
     }
