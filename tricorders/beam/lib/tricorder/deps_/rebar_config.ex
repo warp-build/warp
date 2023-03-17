1000c05da5f9c68b66797a9c4b411765befe0091
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-16 23:32:15 -0300

    feat(tricorders/beam): add support for rebar_config dependencies

---
+++ tricorders/beam/lib/tricorder/deps_/rebar_config.ex
@@ -0,0 +1,29 @@
+defmodule Tricorder.Deps.RebarConfig do
+  def load(file) do
+    with {:ok, conf} <- :file.consult(file) do
+      conf = conf |> :proplists.to_map()
+
+      all_deps = profile_deps(conf) ++ deps(conf)
+
+      all_deps |> Enum.map(&clean/1) |> Enum.into(%{})
+    else
+      _ -> %{}
+    end
+  end
+
+  defp clean({name, spec}), do: {name, Tricorder.Deps.Spec.parse(name, spec)}
+
+  defp deps(conf) do
+    conf |> Map.get(:deps, [])
+  end
+
+  defp profile_deps(conf) do
+    profiles = conf |> Map.get(:profiles, [])
+
+    profiles
+    |> Enum.flat_map(fn {_name, profile} ->
+      profile = profile |> :proplists.to_map()
+      Map.get(profile, :deps, [])
+    end)
+  end
+end
