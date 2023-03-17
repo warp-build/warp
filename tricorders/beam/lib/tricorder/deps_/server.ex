9c837fecb80fd46967d32e6664d666c8e1ac922c
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-16 22:58:09 -0300

    chore(tricorders/beam): snafu with gitignore

---
+++ tricorders/beam/lib/tricorder/deps_/server.ex
@@ -0,0 +1,68 @@
+defmodule Tricorder.Deps.Server do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  ## Callbacks
+
+  @impl true
+  def init(root) do
+    gitignore = Gitignore.read(root)
+
+    {:ok,
+     %{
+       root: root,
+       gitignore: gitignore,
+       deps: %{},
+       finish_scanning: false
+     }}
+  end
+
+  @impl true
+  def handle_call(:loaded?, _from, state) do
+    {:reply, state.finish_scanning, state}
+  end
+
+  @impl true
+  def handle_call(:get_all, _from, state) do
+    {:reply, state.deps, state}
+  end
+
+  @impl true
+  def handle_call({:get, name}, _from, state) do
+    res =
+      case state.deps[name] do
+        nil -> {:error, :not_found}
+        dep -> {:ok, dep}
+      end
+
+    {:reply, res, state}
+  end
+
+  @impl true
+  def handle_cast(:scan, state) do
+    deps =
+      Gitignore.find(
+        state.gitignore,
+        state.root,
+        fn file ->
+          case Path.basename(file) do
+            "mix.lock" -> {:keep, Tricorder.Dep.MixLock.load(file)}
+            "rebar.lock" -> {:keep, Tricorder.Dep.RebarLock.load(file)}
+            "rebar.config" -> {:keep, Tricorder.Dep.RebarConfig.load(file)}
+            _ -> :skip
+          end
+        end
+      )
+      |> Enum.reduce(state.deps, &Map.merge/2)
+
+    state = %{state | deps: deps, finish_scanning: true}
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_cast(_msg, state), do: {:noreply, state}
+end
