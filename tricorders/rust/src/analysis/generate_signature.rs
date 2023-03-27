ad07ec4a9389049f09a85995098157d02863427b
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-27 18:31:51 -0300

    fix(tricorders/rust): sandbox test and fix input file

--- tricorders/rust/src/analysis/generate_signature.rs
+++ tricorders/rust/src/analysis/generate_signature.rs
@@ -21,29 +21,30 @@ impl GenerateSignature {
         workspace_root: String,
         file: String,
     ) -> Result<Vec<Signature>, GenerateSignatureError> {
-        let path = Path::new(&workspace_root).join(&file);
-
-        let file_name = path.file_name().unwrap().to_str().unwrap();
-        let mut mod_paths: Vec<PathBuf> = vec![PathBuf::from(file_name.to_string())];
-        // First we need to read the file contents
-        let source = fs::read_to_string(&path)
-            .await
-            .map_err(|err| GenerateSignatureError::CouldNotReadFile {
-                file: file.to_string(),
-                err,
-            })
-            .unwrap();
-
-        // With the file contents, we try to find anything external to the file
+        // NOTE(@Capitanu): With the file contents, we try to find anything external to the file
         // that we might need. That is, either external modules declared with `mod`
         // or external crates that the file needs. For imported mods, we need
         // to also figure out if they reside in <mod_name>.rs or <mod_name>/mod.rs
-        let (mods, _crates) = TreeSplitter::get_deps_all(&source);
+        let (mods, _crates) = {
+            let path = Path::new(&workspace_root).join(&file);
+            // First we need to read the file contents
+            let source = fs::read_to_string(&path)
+                .await
+                .map_err(|err| GenerateSignatureError::CouldNotReadFile {
+                    file: file.to_string(),
+                    err,
+                })
+                .unwrap();
+            TreeSplitter::get_deps_all(&source)
+        };
 
-        let file_path = path.parent().unwrap();
+        let file_path = Path::new(&file);
+        let file_dir = file_path.parent().unwrap();
+        let file_name = Path::new(file_path.file_name().unwrap().to_str().unwrap()).to_path_buf();
 
-        let mod_files: Vec<PathBuf> = Self::find_mod_files(mods, file_path).await?;
-        mod_paths.extend(mod_files.iter().cloned());
+        let mut mod_paths = vec![file_name];
+        let mod_files = Self::find_mod_files(mods, file_dir).await?;
+        mod_paths.extend(mod_files);
 
         let mut config = Config::default();
         config.insert(
@@ -73,13 +74,13 @@ impl GenerateSignature {
         let mut mod_files: Vec<PathBuf> = Vec::new();
         for m in mods.iter() {
             let mod_dir = Path::new("mod");
-            let path1 = path.join(Path::new(m).with_extension("rs").to_path_buf());
-            let path2 = path.join(mod_dir.join(m).with_extension("rs").to_path_buf());
-            if let Ok(_) = fs::metadata(&path1).await {
+            let path1 = path.join(Path::new(m).with_extension("rs"));
+            let path2 = path.join(mod_dir.join(m).with_extension("rs"));
+            if fs::metadata(&path1).await.is_ok() {
                 mod_files.push(path1.file_name().unwrap().into());
                 continue;
             }
-            if let Ok(_) = fs::metadata(&path2).await {
+            if fs::metadata(&path2).await.is_ok() {
                 mod_files.push(path2.file_name().unwrap().into());
                 continue;
             }
--- tricorders/rust/tests/get_deps_tests.rs
+++ tricorders/rust/tests/get_deps_tests.rs
@@ -1,17 +1,30 @@
+use assert_fs::prelude::*;
 use tricorder::{GenerateSignature, TreeSplitter};
 
 #[tokio::test]
 async fn t002_get_mods() {
+    let curr_workspace = assert_fs::TempDir::new().unwrap();
+
+    let sources = curr_workspace.child("./tests/sources/");
+    sources.copy_from("./tests/sources/", &["*.rs"]).unwrap();
+    let test_file = sources.child("t002_get_mods.rs");
+
     let src = include_str!("./sources/t002_get_mods.rs");
-    let file_path = "./tests/sources/t002_get_mods.rs";
-    let workspace_root = "/Users/capitanu/repos/github.com/warp-build/warp/tricorders/rust/";
     let (mods, crates) = TreeSplitter::get_deps_all(src);
     let ast = TreeSplitter::get_ast(src);
     insta::assert_display_snapshot!(format!("{:#?}", &mods));
     insta::assert_display_snapshot!(format!("{:#?}", &crates));
     insta::assert_display_snapshot!(format!("{:#?}", &ast));
 
-    let signatures =
-        GenerateSignature::all(workspace_root.to_string(), file_path.to_string()).await;
+    let signatures = GenerateSignature::all(
+        curr_workspace.path().to_string_lossy().to_string(),
+        test_file
+            .path()
+            .strip_prefix(curr_workspace.path())
+            .unwrap()
+            .to_string_lossy()
+            .to_string(),
+    )
+    .await;
     insta::assert_display_snapshot!(format!("{:#?}", &signatures));
 }
