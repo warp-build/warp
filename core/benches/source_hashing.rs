cc30f91e1af3441336c88c8a70cec3e17ff8c2f1
Leandro Ostera <leandro@abstractmachines.dev>
2023-03-28 19:16:28 -0300

    fix(benches): use chonky file against smol file

--- core/benches/source_hashing.rs
+++ core/benches/source_hashing.rs
@@ -8,7 +8,7 @@ pub fn source_hasher(c: &mut Criterion) {
     });
     c.bench_function("SourceHasher hash large file", |b| {
         b.to_async(tokio::runtime::Runtime::new().unwrap())
-            .iter(|| SourceHasher::hash("./fixtures/smol.erl"))
+            .iter(|| SourceHasher::hash("./fixtures/chonky.erl"))
     });
 }
 
