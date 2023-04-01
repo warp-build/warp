67df26204909871c5d77c7063fe7de31eea4fc66
Leandro Ostera <leandro@abstractmachines.dev>
2023-04-01 12:30:00 -0300

    feat(tricorders/beam): allow matching tests with different pre/suffixes

--- tricorders/beam/lib/tricorder/analysis/test_matcher.ex
+++ tricorders/beam/lib/tricorder/analysis/test_matcher.ex
@@ -1,11 +1,13 @@
 defmodule Tricorder.Analysis.TestMatcher do
-  def all, do: {:match, ["prop_", "_test"]}
+  def all, do: :all
   def match(parts) when is_list(parts), do: {:match, parts}
   def match(part), do: {:match, [part]}
 
   def from_parts(%{raw: []}), do: all()
   def from_parts(%{raw: raw}), do: {:match, raw}
   def from_parts(_), do: all()
+
+  def matches?(:all, _), do: true
 
   def matches?({:match, raw}, v) when is_binary(v) or is_list(v) do
     String.starts_with?(v, raw) || String.ends_with?(v, raw)
